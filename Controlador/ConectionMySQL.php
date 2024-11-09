ffee19f72cb004b5b2bc0624a68e4e5f440e0080
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-11-09 01:59:03 -0500

    Update ConectionMySQL.php

--- Controlador/ConectionMySQL.php
+++ Controlador/ConectionMySQL.php
@@ -1,17 +1,15 @@
 <?php
-// Datos de conexión a la base de datos
-$servidor = "localhost";  // Usualmente es "localhost" en XAMPP
-$usuario = "root";        // Usuario por defecto en XAMPP es "root"
-$password = "";           // Contraseña por defecto en XAMPP es vacía
-$base_datos = "soccer-clothing"; // Reemplaza con el nombre de tu base de datos
+$host = 'localhost';  // o el host que estés utilizando
+$dbname = 'soccer-clothing';  // nombre de tu base de datos
+$username = 'root';  // tu usuario de la base de datos
+$password = '';  // tu contraseña
 
-// Crear la conexión
-$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
-
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-} else {
-    echo "Conexión exitosa a la base de datos<br>";
+try {
+    // Crea la conexión PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Configura el modo de error
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Conexión fallida: " . $e->getMessage());
 }
 ?>
