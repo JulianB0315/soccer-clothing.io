d8ee042bc4549d111aa37a9b74dfd684172ce7c9
PetusoTwo <1566258@senati.pe>
2024-11-12 13:10:47 -0500

    Estilos arreglado para el Usuario

--- Vista/editUser.php
+++ Vista/editUser.php
@@ -34,26 +34,24 @@ try {
 } catch (PDOException $e) {
     echo "Error al consultar la base de datos: " . $e->getMessage();
 }
-?>
+?> 
 
 
 
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Perfil - Futbolera</title>
-    <link href="./styles.css" rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRP8YB1AC1sn+OAAdhb/I1mfxIp4Hy7jw/DvvuObC" crossorigin="anonymous">
-    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Futbolera</title>
+    <link rel="icon" href="./imgs/logo-tienda.webp">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -112,132 +110,117 @@ try {
                     </div>
                 </div>
             </section>
+
         </div>
     </nav>
-    <!-- Comienzo de la pagina de editar perfil -->
-    <div id="layoutSidenav_content">
-        <main>
-            <header class="page-header page-header-compact page-header-light border-bottom bg-white mb-4">
-                <div class="container-xl px-4">
-                    <div class="page-header-content">
-                        <div class="row align-items-center justify-content-between pt-3">
-                            <div class="col-auto mb-3">
-                                <h1 class="page-header-title">
-                                    <div class="page-header-icon"><i data-feather="user"></i></div>
-                                    Configuracion - Perfil
-                                </h1>
+    <!-- Main -->
+    <main>
+        <!-- Comienzo de la pagina de editar perfil -->
+        <div id="layoutSidenav_content" class="bg-light pt-4" >
+            <main>
+                <header class="page-header page-header-compact page-header-light border-bottom bg-white mb-4">
+                    <div class="container-xl px-4">
+                        <div class="page-header-content">
+                            <div class="row align-items-center justify-content-between pt-3">
+                                <div class="col-auto mb-3">
+                                    <h1 class="page-header-title">
+                                        <div class="page-header-icon"><i data-feather="user"></i></div>
+                                        Configuracion - Perfil
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <!-- Main page content-->
+                <div class="container-xl px-4 mt-4">
+                    <!-- Account page navigation-->
+                    <nav class="nav nav-borders">
+                        <a class="nav-link active ms-0" href="#">Perfil</a>
+                        <a class="nav-link" href="#">Historial</a>
+                    </nav>
+                    <hr class="mt-0 mb-4" />
+                    <div class="row">
+                        <div class="col-xl-4">
+                            <!-- Profile picture card-->
+                            <div class="card mb-4 mb-xl-0">
+                                <div class="card-header">Foto de Perfil</div>
+                                <div class="card-body text-center">
+                                    <!-- Profile picture image-->
+                                    <img class="img-account-profile rounded-circle mb-2" id="profilePic" src="<?php echo $imagenPerfil; ?>" alt="" />
+                                    <!-- Profile picture help block-->
+                                    <div class="small font-italic text-muted mb-4">JPG o PNG no mayor de 5 MB</div>
+                                    <!-- Profile picture upload form-->
+                                    <form action="upload_image.php" method="POST" enctype="multipart/form-data">
+                                        <!-- File input for image -->
+                                        <input type="file" name="profile_image" accept="image/jpeg, image/png" class="form-control mb-3" required />
+                                        <!-- Submit button -->
+                                        <button class="btn btn-primary" type="submit">Sube una imagen</button>
+                                    </form>
+                                </div>
+    
+                            </div>
+                        </div>
+                        <div class="col-xl-8">
+                            <!-- Account details card-->
+                            <div class="mb-4">
+                                <div class="card-header">Detalles de la Cuenta</div>
+                                <div class="card-body">
+                                    <form method="post" action="guardar_cambios_user.php">
+                                        <!-- Form Group (username)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputUsername">Nombre de Usuario:</label>
+                                            <input class="form-control" id="inputUsername" type="text" placeholder="Ingresa tu nombre de usuario" value="<?php echo htmlspecialchars($cliente['apodo']); ?>" disabled />
+                                        </div>
+                                        <!-- Form Row-->
+                                        <div class="row gx-3 mb-3">
+                                            <!-- Form Group (first name)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputFirstName">Nombre:</label>
+                                                <input class="form-control" id="inputFirstName" type="text" placeholder="Ingresa tu nombre" value="<?php echo htmlspecialchars($cliente['nombres']); ?>" disabled />
+                                            </div>
+                                            <!-- Form Group (last name)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputLastName">Apellido:</label>
+                                                <input class="form-control" id="inputLastName" type="text" placeholder="Ingresa tu apellido" value="<?php echo htmlspecialchars($cliente['apellido']); ?>" disabled />
+                                            </div>
+                                        </div>
+                                        <!-- Form Row-->
+                                        <div class="row gx-3 mb-3">
+                                            <!-- Form Group (location)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputLocation">Ubicación:</label>
+                                                <input class="form-control" id="inputLocation" type="text" placeholder="Ingresa tu ubicacion" value="<?php echo htmlspecialchars($cliente['direccion']); ?>" disabled />
+                                            </div>
+                                        </div>
+                                        <!-- Form Group (email address)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputEmailAddress">Correo Electronico:</label>
+                                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Ingresa tu email" value="<?php echo htmlspecialchars($cliente['email']); ?>" disabled />
+                                        </div>
+                                        <!-- Form Row-->
+                                        <div class="row gx-3 mb-3">
+                                            <!-- Form Group (phone number)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputPhone">Numero telefonico:</label>
+                                                <input class="form-control" id="inputPhone" type="tel" placeholder="Ingresa tu numero" value="<?php echo htmlspecialchars($cliente['telefono']); ?>" disabled />
+                                            </div>
+                                            <!-- Form Group (birthday)-->
+                                        </div>
+                                        <!--Save changes button-->
+                                        <button class="btn btn-primary me-3" type="button" onclick="habilitarCampos()">Editar perfil</button>
+                                        <button class="btn btn-primary" type="button" id="guardarCambios" disabled onclick="deshabilitarCampos()">Guardar cambios</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </header>
-            <!-- Main page content-->
-            <div class="container-xl px-4 mt-4">
-                <!-- Account page navigation-->
-                <nav class="nav nav-borders">
-                    <a class="nav-link active ms-0" href="#">Perfil</a>
-                    <a class="nav-link" href="#">Historial</a>
-                </nav>
-                <hr class="mt-0 mb-4" />
-                <div class="row">
-                    <div class="col-xl-4">
-                        <!-- Profile picture card-->
-                        <div class="card mb-4 mb-xl-0">
-                            <div class="card-header">Foto de Perfil</div>
-                            <div class="card-body text-center">
-                                <!-- Profile picture image-->
-                                <img class="img-account-profile rounded-circle mb-2" id="profilePic" src="<?php echo $imagenPerfil; ?>" alt="" />
-                                <!-- Profile picture help block-->
-                                <div class="small font-italic text-muted mb-4">JPG o PNG no mayor de 5 MB</div>
-                                <!-- Profile picture upload form-->
-                                <form action="upload_image.php" method="POST" enctype="multipart/form-data">
-                                    <!-- File input for image -->
-                                    <input type="file" name="profile_image" accept="image/jpeg, image/png" class="form-control mb-3" required />
-                                    <!-- Submit button -->
-                                    <button class="btn btn-primary" type="submit">Sube una imagen</button>
-                                </form>
-                            </div>
+            </main>
+        </div>
+    </main>
 
-                        </div>
-                    </div>
-                    <div class="col-xl-8">
-                        <!-- Account details card-->
-                        <div class="mb-4">
-                            <div class="card-header">Detalles de la Cuenta</div>
-                            <div class="card-body">
-                                <form method="post" action="guardar_cambios_user.php">
-                                    <!-- Form Group (username)-->
-                                    <div class="mb-3">
-                                        <label class="small mb-1" for="inputUsername">Nombre de Usuario:</label>
-                                        <input class="form-control" id="inputUsername" type="text" placeholder="Ingresa tu nombre de usuario" value="<?php echo htmlspecialchars($cliente['apodo']); ?>" disabled />
-                                    </div>
-                                    <!-- Form Row-->
-                                    <div class="row gx-3 mb-3">
-                                        <!-- Form Group (first name)-->
-                                        <div class="col-md-6">
-                                            <label class="small mb-1" for="inputFirstName">Nombre:</label>
-                                            <input class="form-control" id="inputFirstName" type="text" placeholder="Ingresa tu nombre" value="<?php echo htmlspecialchars($cliente['nombres']); ?>" disabled />
-                                        </div>
-                                        <!-- Form Group (last name)-->
-                                        <div class="col-md-6">
-                                            <label class="small mb-1" for="inputLastName">Apellido:</label>
-                                            <input class="form-control" id="inputLastName" type="text" placeholder="Ingresa tu apellido" value="<?php echo htmlspecialchars($cliente['apellido']); ?>" disabled />
-                                        </div>
-                                    </div>
-                                    <!-- Form Row-->
-                                    <div class="row gx-3 mb-3">
-                                        <!-- Form Group (location)-->
-                                        <div class="col-md-6">
-                                            <label class="small mb-1" for="inputLocation">Ubicación:</label>
-                                            <input class="form-control" id="inputLocation" type="text" placeholder="Ingresa tu ubicacion" value="<?php echo htmlspecialchars($cliente['direccion']); ?>" disabled />
-                                        </div>
-                                    </div>
-                                    <!-- Form Group (email address)-->
-                                    <div class="mb-3">
-                                        <label class="small mb-1" for="inputEmailAddress">Correo Electronico:</label>
-                                        <input class="form-control" id="inputEmailAddress" type="email" placeholder="Ingresa tu email" value="<?php echo htmlspecialchars($cliente['email']); ?>" disabled />
-                                    </div>
-                                    <!-- Form Row-->
-                                    <div class="row gx-3 mb-3">
-                                        <!-- Form Group (phone number)-->
-                                        <div class="col-md-6">
-                                            <label class="small mb-1" for="inputPhone">Numero telefonico:</label>
-                                            <input class="form-control" id="inputPhone" type="tel" placeholder="Ingresa tu numero" value="<?php echo htmlspecialchars($cliente['telefono']); ?>" disabled />
-                                        </div>
-                                        <!-- Form Group (birthday)-->
-                                    </div>
-                                    <!--Save changes button-->
-                                    <button class="btn btn-primary me-3" type="button" onclick="habilitarCampos()">Editar perfil</button>
-                                    <button class="btn btn-primary" type="button" id="guardarCambios" disabled onclick="deshabilitarCampos()">Guardar cambios</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
-
-
-    <script>
-        // Función para habilitar los cosos del form
-        function habilitarCampos() {
-            const inputs = document.querySelectorAll('input.form-control');
-            inputs.forEach(input => input.disabled = false);
-
-            // Se supone q con este se habilita el para guardar cambios
-            document.getElementById('guardarCambios').disabled = false;
-        }
-
-        // Función para deshabilitar
-        function deshabilitarCampos() {
-            const inputs = document.querySelectorAll('input.form-control');
-            inputs.forEach(input => input.disabled = true);
-
-            // Se supone q con este se deshabilita el para guardar cambios
-            document.getElementById('guardarCambios').disabled = true;
-        }
-    </script>
+    </main>
 
     <!-- Pie de pagina / Footer -->
     <footer class="footer">
@@ -287,9 +270,7 @@ try {
             <p>&copy; 2024 Futbolera. Todos los derechos reservados.</p>
         </div>
     </footer>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="carAlert.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
